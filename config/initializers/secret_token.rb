f74ff8481c2831c455070c9acd19123c70b363d8
Nawal Behih <nawalbehih@dyn-86-36-42-77.wv.qatar.cmu.edu>
2012-11-24 10:34:29 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OTPSMaster::Application.config.secret_token = '8fd49092a5d7faead882cdf7f21573ae1786cd2ec2234e5f9f21dfc639b9fec3b3fd9d25ca625e8f4e507332915ea38c3a4ab1d4af8f619869e83e6900d3a179'
